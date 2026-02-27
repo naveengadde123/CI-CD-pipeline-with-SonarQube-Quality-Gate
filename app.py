303c01874e3abbf4258f7dc838b2f2d0c83ff561
Naveen Gadde <naveen.gadde@vkraftsoftware.com>
2026-02-27 12:35:58 +0530

    updated app.py file

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+password = "admin123"   # Hardcoded credential (Security issue)
+
 @app.route("/")
 def home():
+    unused_variable = 100  # Code smell (unused variable)
     return jsonify({"message": "Flask SonarQube Integration Working!"})
 
 @app.route("/add", methods=["POST"])
@@ -12,6 +15,18 @@ def add():
     a = data.get("a", 0)
     b = data.get("b", 0)
     return jsonify({"result": a + b})
+
+@app.route("/divide")
+def divide():
+    a = 10
+    b = 0
+    return jsonify({"result": a / b})  # Division by zero (Bug)
+
+@app.route("/danger")
+def danger():
+    user_input = request.args.get("code")
+    eval(user_input)   # Security vulnerability (Use of eval)
+    return "Executed"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- test_app.py
+++ test_app.py
@@ -1,32 +1,11 @@
-from flask import Flask, jsonify, request
+from app import app
 
-app = Flask(__name__)
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
 
-password = "admin123"   # Hardcoded credential (Security issue)
-
-@app.route("/")
-def home():
-    unused_variable = 100  # Code smell (unused variable)
-    return jsonify({"message": "Flask SonarQube Integration Working!"})
-
-@app.route("/add", methods=["POST"])
-def add():
-    data = request.get_json()
-    a = data.get("a", 0)
-    b = data.get("b", 0)
-    return jsonify({"result": a + b})
-
-@app.route("/divide")
-def divide():
-    a = 10
-    b = 0
-    return jsonify({"result": a / b})  # Division by zero (Bug)
-
-@app.route("/danger")
-def danger():
-    user_input = request.args.get("code")
-    eval(user_input)   # Security vulnerability (Use of eval)
-    return "Executed"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+def test_add():
+    client = app.test_client()
+    response = client.post("/add", json={"a": 5, "b": 3})
+    assert response.json["result"] == 8
